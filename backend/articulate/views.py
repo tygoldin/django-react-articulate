c3b15603f704784be10c05a093cf7fc5d8cdda69
Srikrishnan Veeraraghavan <v.sriku@gmail.com>
2022-03-21 13:30:52 -0400

    add reco system and color pallete api

--- backend/articulate/views.py
+++ backend/articulate/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 import csv
 import pandas as pd
+import random
 # Create your views here.
 
 
@@ -58,3 +59,70 @@ def insert_artworks(request):
 
                 )
         return Response(None)
+
+@api_view(['GET', 'POST'])
+def insert_artworks_desc_colors(request):
+    if request.method == 'GET':
+        Artwork.objects.all().delete()
+        with open('catalog_coord_keyword_color.csv', encoding='latin1') as f:
+            reader = csv.reader(f)
+            next(reader)
+            for row in reader:
+                #Handling NA values in lat-long
+                if row[11] == '#N/A':
+                    row[11] = 0
+                if row[12] == '#N/A':
+                    row[12] = 0
+
+                _, created = Artwork.objects.get_or_create(
+                    author=row[0],
+                    born_died=row[1],
+                    title=row[2],
+                    date=row[3],
+                    technique=row[4],
+                    location=row[5],
+                    url=row[6],
+                    form=row[7],
+                    type=row[8],
+                    school=row[9],
+                    timeframe=row[10],
+                    lat=row[11],
+                    lng=row[12],
+                    descriptions=row[13],
+                    keywords=row[14],
+                    color_0=row[15],
+                    color_1=row[16],
+                    color_2=row[17],
+                    color_3=row[18],
+                    color_4=row[19],
+                    color_5=row[20],
+                    cluster_id=row[21]
+
+                )
+        return Response(None)
+
+@api_view(['GET', 'POST'])
+def test_route(request):
+    if request.method == 'GET':
+        art1 = Artwork.objects.filter(id=174331).values()
+        return Response(art1)
+
+
+@api_view(['GET', 'POST'])
+def get_recommendations(request):
+    if request.method == 'GET':
+        artwork_id = request.GET.get('artwork_id', '')
+        current_art = Artwork.objects.filter(id=artwork_id).values()[0]
+        current_cluster = current_art['cluster_id']
+        
+        cluster_artworks = Artwork.objects.filter(cluster_id=current_cluster).values()
+        recos = random.sample(list(cluster_artworks),10)
+        return Response([art['id'] for art in recos])
+
+
+@api_view(['GET', 'POST'])
+def get_color_palette(request):
+    if request.method == 'GET':
+        artwork_id = request.GET.get('artwork_id', '')
+        current_art = Artwork.objects.filter(id=artwork_id).values()[0]
+        return Response({key: value for key, value in current_art.items() if key.startswith('color')})
