82daff75e6d1e78e67850aaf4c122e82e589043d
tygoldin <54474684+tygoldin@users.noreply.github.com>
2022-04-17 19:43:27 -0400

    Added interaction model and serializer

--- backend/articulate/serializers.py
+++ backend/articulate/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Artwork
+from .models import Artwork, Interactions
 
 
 class ArtworkSerializer(serializers.ModelSerializer):
@@ -8,3 +8,8 @@ class ArtworkSerializer(serializers.ModelSerializer):
         fields = ('id', 'author', 'born_died', 'title', 'date', 'technique',
                   'location', 'url', 'form', 'type', 'school', 'timeframe', 'lat', 'lng',
                   'descriptions','keywords','color_0','color_1','color_2','color_3','color_4','color_5','cluster_id')
+
+class InteractionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Interactions
+        fields = ('id', 'user', 'view_count', 'artwork', 'liked')
